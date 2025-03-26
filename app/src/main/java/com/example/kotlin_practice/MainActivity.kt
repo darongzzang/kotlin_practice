de54efe44f36720a680d557bf026619c12c7749b
darongzzang <101727090+darongzzang@users.noreply.github.com>
2025-03-26 23:19:50 +0900

    3주차 과제 - 삭제버튼 추가

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kotlin_practice
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val edit = findViewById<EditText>(R.id.edit)
+        val btn = findViewById<Button>(R.id.btn)
+        val tv = findViewById<TextView>(R.id.tv)
+        val xbtn = findViewById<Button>(R.id.xbtn)
+
+        btn.setOnClickListener {
+            tv.text = edit.text.toString()
+        }
+
+        xbtn.setOnClickListener {
+            edit.text.clear()  // EditText 문자열 삭제
+            tv.text = ""       // TextView 내용 삭제
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
